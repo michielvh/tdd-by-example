cd0c98a0a2d88acc123975d0eae48235159184ed
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-19 12:58:51 +0200

    #2

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -4,14 +4,16 @@ public class Dollar extends Money {
 
 
 
-    public Dollar(int amount){
+    public Dollar(int amount, String currency) {
+        super(amount,currency);
 
-        this.amount=amount;
     }
 
-    public Dollar times(int multiplier){
 
-        return new Dollar(amount*multiplier);
+
+    public Money times(int multiplier){
+
+        return Money.dollar(amount*multiplier);
     }
 
 
--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -3,14 +3,17 @@ package guru.springframework;
 public class Franc extends Money{
 
 
-    public Franc(int amount){
+    public Franc(int amount,String currency) {
+        super(amount,currency);
 
-        this.amount=amount;
     }
 
-    public Franc times(int multiplier){
 
-        return new Franc(amount*multiplier);
+
+
+    public Money times(int multiplier){
+
+        return Money.franc(amount*multiplier);
     }
 
 
